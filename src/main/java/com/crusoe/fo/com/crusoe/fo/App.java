46413fb0ad2ade1ea9ea57866fbde57379665092
tomcat.gu <tomcat.gu@gmail.com>
2018-12-14 15:45:50 +0800

    Create...

---
+++ src/main/java/com/crusoe/fo/com/crusoe/fo/App.java
@@ -0,0 +1,13 @@
+package com.crusoe.fo.com.crusoe.fo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
