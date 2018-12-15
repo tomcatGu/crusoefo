3c79b3cfe4ed3a67fbf645b726778a8dfc46002e
tomcatGu <tomcat.Gu@gmail.com>
2018-12-15 15:45:59 +0800

    init flowable

--- src/main/java/com/crusoe/fo/com/crusoe/fo/App.java
+++ src/main/java/com/crusoe/fo/com/crusoe/fo/App.java
@@ -1,13 +1,19 @@
 package com.crusoe.fo.com.crusoe.fo;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+@ComponentScan
+
+@EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
