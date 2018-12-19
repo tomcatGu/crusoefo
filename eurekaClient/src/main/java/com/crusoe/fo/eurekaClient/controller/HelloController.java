b3cc371d8a16d5073579f176ec59fe0a71c8c386
tomcat.gu <tomcat.gu@gmail.com>
2018-12-19 16:56:08 +0800

    add helloController.java

---
+++ eurekaClient/src/main/java/com/crusoe/fo/eurekaClient/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.crusoe.fo.eurekaClient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(@RequestParam String name) {
+		
+		return "hello,"+name;
+	}
+
+}
