8fade3245d10572cd5cd9e45ae22850f66a538a6
tomcat.gu <tomcat.gu@gmail.com>
2018-12-20 15:57:09 +0800

    test gateway-service

---
+++ gateway-service/src/main/java/com/crusoe/fo/gatewayservice/App.java
@@ -0,0 +1,28 @@
+package com.crusoe.fo.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+
+
+}
