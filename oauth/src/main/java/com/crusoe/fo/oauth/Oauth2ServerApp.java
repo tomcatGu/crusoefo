75eb1227691f5c8149b6076888e69a10769e6419
tomcatGu <tomcat.Gu@gmail.com>
2019-01-14 20:51:42 +0800

    It's so strange. additionalInformation.put("user_name", authentication.getDetails()); would caused an error "Only one connection receive subscriber allowed." additionalInformation.put("user_name", authentication.getDetails()); it works well.

--- oauth/src/main/java/com/crusoe/fo/oauth/Oauth2ServerApp.java
+++ oauth/src/main/java/com/crusoe/fo/oauth/Oauth2ServerApp.java
@@ -2,11 +2,13 @@ package com.crusoe.fo.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-public class Oauth2ServerApp {
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2ServerApp.class, args);
-	}
+public class
+Oauth2ServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerApp.class, args);
+    }
+
+
 }
--- oauth/src/main/java/com/crusoe/fo/oauth/ResourceServerConfiguration.java
+++ oauth/src/main/java/com/crusoe/fo/oauth/ResourceServerConfiguration.java
@@ -1,13 +1,7 @@
 package com.crusoe.fo.oauth;
 
-import java.io.IOException;
-import java.security.KeyPair;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -15,9 +9,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import io.micrometer.core.instrument.util.IOUtils;
-import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 
 @Configuration
 
@@ -28,13 +19,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Autowired
     JwtAccessTokenConverter jwtAccessTokenConverter;
 
-    @Override
-
-    public void configure(ResourceServerSecurityConfigurer resources) {
-
-        resources.resourceId(DEMO_RESOURCE_ID).stateless(true).tokenStore(new JwtTokenStore(jwtAccessTokenConverter));
-
-    }
 
     @Override
 
@@ -44,9 +28,15 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 // .successHandler(appLoginInSuccessHandler))//如果有必要，在这里可以自定义成功处理器
                 // .failureHandler(appLoginFailureHandler)//如果有必要，在这里可以自定义错误处理器
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().authorizeRequests()
-                .antMatchers("/open/**","/login**").permitAll()// 开放的资源不用授权
+                .antMatchers("/open/**", "/login**").permitAll()// 开放的资源不用授权
                 .anyRequest().authenticated();// 其他任何请求都需要授权
 
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+
+        resources.resourceId(DEMO_RESOURCE_ID).stateless(true).tokenStore(new JwtTokenStore(jwtAccessTokenConverter));
     }
 
 }
