82ad80dbd9b1b8a79294380629efe7285a72fb5f
tomcatGu <ntgwx@qq.com>
2019-06-13 15:32:09 +0800

    add sidecar to integrate a nodejs app

---
+++ eurekasidecar/src/main/java/com/crusoe/fo/eurekasidecar/SidecarServerApplication.java
@@ -0,0 +1,15 @@
+package com.crusoe.fo.eurekasidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class SidecarServerApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(SidecarServerApplication.class, args);
+    }
+}
