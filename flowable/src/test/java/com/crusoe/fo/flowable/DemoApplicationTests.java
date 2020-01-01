3554188a1d71a8716981ab24ed28aeb7b0cd28d8
tomcatGu <tomcatGu@google.com>
2020-01-01 13:04:16 +0800

    update flowable

---
+++ flowable/src/test/java/com/crusoe/fo/flowable/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crusoe.fo.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
