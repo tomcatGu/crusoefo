1c5d3a75c732d59c67c495729bb2ff6466420970
tomcatGu <tomcatGu@google.com>
2020-01-06 11:40:03 +0800

    add flowable modeler

--- flowable/src/main/java/com/crusoe/fo/flowable/DemoApplication.java
+++ flowable/src/main/java/com/crusoe/fo/flowable/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.crusoe.fo.flowable;
-
+import org.flowable.ui.modeler.conf.ApplicationConfiguration;
+import org.flowable.ui.modeler.servlet.AppDispatcherServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Import;
+@Import({
+	ApplicationConfiguration.class,
+	AppDispatcherServletConfiguration.class
+})
 @SpringBootApplication
 public class DemoApplication {
 
