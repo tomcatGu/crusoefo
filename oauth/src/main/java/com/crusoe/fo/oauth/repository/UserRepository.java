5a6da3ec5339f7eb9b303fdfc03493b92af54c4a
tomcatGu <tomcatGu@google.com>
2020-02-10 13:13:22 +0800

    test docker push

--- oauth/src/main/java/com/crusoe/fo/oauth/repository/UserRepository.java
+++ oauth/src/main/java/com/crusoe/fo/oauth/repository/UserRepository.java
@@ -3,7 +3,8 @@ package com.crusoe.fo.oauth.repository;
 import com.crusoe.fo.oauth.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findByUsername(String username);
