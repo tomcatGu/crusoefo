785610ccbac82365baebba32732a74a8c82bb839
tomcatGu <ntgwx@qq.com>
2020-02-11 17:22:38 +0800

    add start process endpoint

--- flowable-centre/src/main/java/com/crusoe/fo/controller/ExpenseController.java
+++ flowable-centre/src/main/java/com/crusoe/fo/controller/ExpenseController.java
@@ -1,12 +1,27 @@
 package com.crusoe.fo.controller;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.flowable.bpmn.model.BpmnModel;
 import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
+import org.flowable.engine.runtime.Execution;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.image.ProcessDiagramGenerator;
+import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "expense")
@@ -19,6 +34,77 @@ public class ExpenseController {
     private RepositoryService repositoryService;
     @Autowired
     private ProcessEngine processEngine;
- 
-/***************此处为业务代码******************/
+
+    /*************** 此处为业务代码 ******************/
+
+
+    @RequestMapping(value = "start")
+    @ResponseBody
+    public String addExpense(String userId, String processId) {
+        //启动流程
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("taskUser", userId);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processId, map);
+        return "提交成功.流程Id为：" + processInstance.getId();
+    }
+
+    /**
+     * 生成流程图
+     *
+     * @param processId 任务ID
+     */
+    @RequestMapping(value = "processDiagram")
+    public void genProcessDiagram(HttpServletResponse httpServletResponse, String processId) throws Exception {
+        ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(processId).singleResult();
+
+        // 流程走完的不显示图
+        if (pi == null) {
+            return;
+        }
+        Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).singleResult();
+        // 使用流程实例ID，查询正在执行的执行对象表，返回流程实例对象
+        String InstanceId = task.getProcessInstanceId();
+        List<Execution> executions = runtimeService.createExecutionQuery().processInstanceId(InstanceId).list();
+
+        // 得到正在执行的Activity的Id
+        List<String> activityIds = new ArrayList<>();
+        List<String> flows = new ArrayList<>();
+        for (Execution exe : executions) {
+            List<String> ids = runtimeService.getActiveActivityIds(exe.getId());
+            activityIds.addAll(ids);
+        }
+
+        // 获取流程图
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(pi.getProcessDefinitionId());
+        ProcessEngineConfiguration engconf = processEngine.getProcessEngineConfiguration();
+        ProcessDiagramGenerator diagramGenerator = engconf.getProcessDiagramGenerator();
+        InputStream in = diagramGenerator.generateDiagram(bpmnModel, "png", activityIds, flows,
+                engconf.getActivityFontName(), engconf.getLabelFontName(), engconf.getAnnotationFontName(),
+                engconf.getClassLoader(), 1.0, true);
+
+        // 设置响应的类型格式为图片格式
+        httpServletResponse.setContentType("image/png");
+        // 禁止图像缓存。
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setHeader("Cache-Control", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+
+        OutputStream out = null;
+        byte[] buf = new byte[1024];
+        int legth = 0;
+        try {
+            out = httpServletResponse.getOutputStream();
+            while ((legth = in.read(buf)) != -1) {
+                out.write(buf, 0, legth);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
 }
