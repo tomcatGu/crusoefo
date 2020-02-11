0a3e9d43bed71facdb0b99adc6c2daa6fab23aee
tomcatGu <tomcatGu@google.com>
2020-02-11 22:25:53 +0800

    fix publish function

---
+++ flowable-modeler/src/main/java/com/crusoe/fo/flowable/controller/DeploymentController.java
@@ -0,0 +1,171 @@
+package com.crusoe.fo.flowable.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipInputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.flowable.common.engine.api.FlowableException;
+import org.flowable.common.engine.api.FlowableIllegalArgumentException;
+import org.flowable.common.rest.util.RestUrlBuilder;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.DeploymentBuilder;
+import org.flowable.rest.service.api.BpmnRestApiInterceptor;
+import org.flowable.rest.service.api.RestUrls;
+import org.flowable.rest.service.api.repository.DeploymentResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
+
+@RestController
+@RequestMapping(value = "/flowable-task/process-api")
+public class DeploymentController {
+
+//    @Autowired
+//    protected RestResponseFactory restResponseFactory;
+
+    @Autowired
+    protected RepositoryService repositoryService;
+
+    @Autowired(required=false)
+    protected BpmnRestApiInterceptor restApiInterceptor;
+
+
+    /**
+     * 部署（发布） rest api publish中会调用此接口而非直接ui调用
+     * @param deploymentKey
+     * @param deploymentName
+     * @param tenantId
+     * @param request
+     * @param response
+     * @return
+     */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "file", dataType = "file", paramType = "form", required = true)
+    })
+    @PostMapping(value = "/app-repository/deployments", produces = "application/json", consumes = "multipart/form-data")
+    public DeploymentResponse uploadDeployment(@ApiParam(name = "deploymentKey") @RequestParam(value = "deploymentKey", required = false) String deploymentKey,
+                                               @ApiParam(name = "deploymentName") @RequestParam(value = "deploymentName", required = false) String deploymentName,
+                                               @ApiParam(name = "tenantId") @RequestParam(value = "tenantId", required = false) String tenantId,
+                                               HttpServletRequest request, HttpServletResponse response) {
+
+        if (!(request instanceof MultipartHttpServletRequest)) {
+            throw new FlowableIllegalArgumentException("Multipart request is required");
+        }
+
+        if (restApiInterceptor != null) {
+            restApiInterceptor.executeNewDeploymentForTenantId(tenantId);
+        }
+
+        String queryString = request.getQueryString();
+        Map<String, String> decodedQueryStrings = splitQueryString(queryString);
+
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+
+        if (multipartRequest.getFileMap().size() == 0) {
+            throw new FlowableIllegalArgumentException("Multipart request with file content is required");
+        }
+
+        MultipartFile file = multipartRequest.getFileMap().values().iterator().next();
+
+        try {
+            DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+            String fileName = file.getOriginalFilename();
+            if (StringUtils.isEmpty(fileName) || !(fileName.endsWith(".bpmn20.xml") || fileName.endsWith(".bpmn") || fileName.toLowerCase().endsWith(".bar") || fileName.toLowerCase().endsWith(".zip"))) {
+
+                fileName = file.getName();
+            }
+
+            if (fileName.endsWith(".bpmn20.xml") || fileName.endsWith(".bpmn")) {
+                deploymentBuilder.addInputStream(fileName, file.getInputStream());
+            } else if (fileName.toLowerCase().endsWith(".bar") || fileName.toLowerCase().endsWith(".zip")) {
+                deploymentBuilder.addZipInputStream(new ZipInputStream(file.getInputStream()));
+            } else {
+                throw new FlowableIllegalArgumentException("File must be of type .bpmn20.xml, .bpmn, .bar or .zip");
+            }
+
+            if (!decodedQueryStrings.containsKey("deploymentName") || StringUtils.isEmpty(decodedQueryStrings.get("deploymentName"))) {
+                String fileNameWithoutExtension = fileName.split("\\.")[0];
+
+                if (StringUtils.isNotEmpty(fileNameWithoutExtension)) {
+                    fileName = fileNameWithoutExtension;
+                }
+
+                deploymentBuilder.name(fileName);
+            } else {
+                deploymentBuilder.name(decodedQueryStrings.get("deploymentName"));
+            }
+
+            if (decodedQueryStrings.containsKey("deploymentKey") && StringUtils.isNotEmpty(decodedQueryStrings.get("deploymentKey"))) {
+                deploymentBuilder.key(decodedQueryStrings.get("deploymentKey"));
+            }
+
+            if (tenantId != null) {
+                deploymentBuilder.tenantId(tenantId);
+            }
+
+            Deployment deployment = deploymentBuilder.deploy();
+
+            response.setStatus(HttpStatus.CREATED.value());
+
+            return createDeploymentResponse(deployment);// restResponseFactory.createDeploymentResponse(deployment);
+
+        } catch (Exception e) {
+            if (e instanceof FlowableException) {
+                throw (FlowableException) e;
+            }
+            throw new FlowableException(e.getMessage(), e);
+        }
+    }
+    public Map<String, String> splitQueryString(String queryString) {
+        if (StringUtils.isEmpty(queryString)) {
+            return Collections.emptyMap();
+        }
+        Map<String, String> queryMap = new HashMap<>();
+        for (String param : queryString.split("&")) {
+            queryMap.put(StringUtils.substringBefore(param, "="), decode(StringUtils.substringAfter(param, "=")));
+        }
+        return queryMap;
+    }
+    protected String decode(String string) {
+        if (string != null) {
+            try {
+                return URLDecoder.decode(string, "UTF-8");
+            } catch (UnsupportedEncodingException uee) {
+                throw new IllegalStateException("JVM does not support UTF-8 encoding.", uee);
+            }
+        }
+        return null;
+    }
+
+
+    public DeploymentResponse createDeploymentResponse(Deployment deployment) {
+        return createDeploymentResponse(deployment, createUrlBuilder());
+    }
+
+    public DeploymentResponse createDeploymentResponse(Deployment deployment, RestUrlBuilder urlBuilder) {
+        return new DeploymentResponse(deployment, urlBuilder.buildUrl(RestUrls.URL_DEPLOYMENT, deployment.getId()));
+    }
+
+    protected RestUrlBuilder createUrlBuilder() {
+        return RestUrlBuilder.fromCurrentRequest();
+    }
+
+}
