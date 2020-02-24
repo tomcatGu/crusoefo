0099adcc8e413edb46596f8285b6f85b82e7b1ff
tomcatGu <tomcatGu@google.com>
2020-02-24 21:32:42 +0800

    test springboot banner

--- camunda-centre/src/main/java/com/crusoe/fo/config/CORSConfiguration.java
+++ camunda-centre/src/main/java/com/crusoe/fo/config/CORSConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
+//@Configuration
 public class CORSConfiguration extends WebMvcConfigurationSupport {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
