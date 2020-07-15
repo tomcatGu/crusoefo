3f2bff83dfb383ee641cafe86be6a7899cb3ac2d
tomcatGu <tomcatGu@google.com>
2020-07-15 21:57:43 +0800

    exclude jsr311-api for consul

--- user-centre/src/main/java/com/crusoe/fo/usercenter/config/UserResourceServerConfig.java
+++ user-centre/src/main/java/com/crusoe/fo/usercenter/config/UserResourceServerConfig.java
@@ -31,6 +31,7 @@ public class UserResourceServerConfig extends ResourceServerConfigurerAdapter{
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
         .authorizeRequests()
+        .antMatchers("/actuator/**").permitAll()
         //.antMatchers("/rest/**").authenticated()
         .antMatchers("/**").authenticated();
     }
