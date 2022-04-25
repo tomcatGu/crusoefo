84a60aa36f65bd9d9e45d0baeacb1f70da181a1c
tomcatGu <ntgwx@qq.com>
2022-04-25 16:17:33 +0800

    add zeebeclient moudle

---
+++ zeebeclient/src/main/java/com/crusoe/fo/App.java
@@ -0,0 +1,25 @@
+package com.crusoe.fo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.camunda.zeebe.client.ZeebeClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+
+    private static final String zeebeAPI = "localhost:26500";
+
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+
+        ZeebeClient client = ZeebeClient.newClientBuilder()
+                .gatewayAddress(zeebeAPI)
+                // .credentialsProvider(credentialsProvider)
+                .usePlaintext()
+                .build();
+    }
+}
