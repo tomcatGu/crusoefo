88bf9086d77327c03371b9189bb061e50109955c
tomcatGu <ntgwx@qq.com>
2022-08-10 17:31:39 +0800

    add zeebecentre module

---
+++ zeebecentre/src/main/java/com/crusoe/zeebecentre/App.java
@@ -0,0 +1,13 @@
+package com.crusoe.zeebecentre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
