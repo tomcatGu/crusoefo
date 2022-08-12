70ced047614830d3e082c930891481fa9678daa0
tomcatGu <ntgwx@qq.com>
2022-08-12 17:38:20 +0800

    enable spring boot application

--- zeebecentre/src/main/java/com/crusoe/zeebecentre/App.java
+++ zeebecentre/src/main/java/com/crusoe/zeebecentre/App.java
@@ -1,9 +1,17 @@
 package com.crusoe.zeebecentre;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+
+@SpringBootApplication
+@EnableZeebeClient
+@EnableScheduling
+@EnableAsync
+@EnableSpringDataWebSupport
 public class App 
 {
     public static void main( String[] args )
